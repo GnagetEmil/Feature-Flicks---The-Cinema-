222626f5546c48f00e33bc94ca3b3c10167d152e
GnagetEmil <emil@gnaget.se>
2023-03-19 16:32:59 +0100

    Added dropdown and filtering by category

--- src/components/Screening.jsx
+++ src/components/Screening.jsx
@@ -3,48 +3,20 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 export default function Screening(props) {
-  const [movieTitle, setMovieTitle] = useState('');
-  const [auditoriumName, setAuditoriumName] = useState('');
-  const [category, setCategories] = useState('');;
-
-  // Get The Movie Title Based On the MovieId 
-  useEffect(() => {
-    fetch(`/api/movies/${props.movieId}`)
-      .then(response => response.json())
-      .then(data => setMovieTitle(data.title))
-      .catch(error => console.log(error));
-  }, [props.movieId]);
-
-  // Get The Auditorium Name Based On the AuditoriumId
-  useEffect(() => {
-    fetch(`/api/auditoriums/${props.auditoriumId}`)
-      .then(response => response.json())
-      .then(data => setAuditoriumName(data.name))
-      .catch(error => console.log(error));
-  }, [props.auditoriumId]);
-
-
-  useEffect(() => {
-    fetch(`/api/movies/${props.movieId}`)
-      .then(response => response.json())
-      .then(data => setCategories(data.description.categories))
-      .catch(error => console.log(error));
-  }, [props.movieId]);
-
   return (
     <div className="movie">
       <Card className="card mx-auto mb-4" style={{ width: '19rem' }}>
         <Card.Body className="text-center">
-          <Card.Title>{movieTitle}</Card.Title>
+          <Card.Title>{props.movieTitle}</Card.Title>
           <Card.Text>
             {props.time}
           </Card.Text>
           <Card.Text>
-            {auditoriumName}
+            {props.auditoriumName}
           </Card.Text>
-          <Card.Text>
-            {category + " "}
-          </Card.Text>
+
+          <Card.Text>{props.category.join(" | ")}</Card.Text>
+
           <Button>Book</Button>
         </Card.Body>
       </Card>
--- src/pages/Screenings.jsx
+++ src/pages/Screenings.jsx
@@ -7,25 +7,87 @@ import Screening from '../components/Screening';
 
 
 const Screenings = () => {
+    const [screeningsData, setScreeningsData] = useState([]);
     const [screenings, setScreenings] = useState([]);
 
     useEffect(() => {
         (async () => {
             const screeningsData = await (await fetch('/api/screenings/?sort=time')).json();
+            setScreeningsData(screeningsData);
             setScreenings(screeningsData);
         })();
     }, []);
 
+    const [movieTitles, setMovieTitles] = useState({});
+    const [auditoriumNames, setAuditoriumNames] = useState({});
+    const [categories, setCategories] = useState({});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const moviesData = await (await fetch('/api/movies')).json();
+            const auditoriumsData = await (await fetch('/api/auditoriums')).json();
+            setMovieTitles(moviesData.reduce((obj, movie) => {
+                obj[movie.id] = movie.title;
+                return obj;
+            }, {}));
+            setAuditoriumNames(auditoriumsData.reduce((obj, auditorium) => {
+                obj[auditorium.id] = auditorium.name;
+                return obj;
+            }, {}));
+            setCategories(moviesData.reduce((obj, movie) => {
+                obj[movie.id] = movie.description.categories;
+                return obj;
+            }, {}));
+        }
+        fetchData();
+    }, []);
+
+
+    const handleCategorySelect = (e) => {
+        const selectedCategory = e.target.value;
+        if (selectedCategory === "all") {
+            setScreenings(screeningsData);
+        } else {
+            const filteredScreenings = screeningsData.filter(
+                (screening) => categories[screening.movieId]?.includes(selectedCategory)
+            );
+            setScreenings(filteredScreenings);
+        }
+    }
+
     return (
         <Container className="text-center">
+            <div className="d-flex justify-content-center my-4">
+                <select
+                    className="form-select"
+                    onChange={handleCategorySelect}
+                >
+                    <option value="all">All categories</option>
+                    {Object.values(categories).flatMap((cats) => cats).filter((cat, i, arr) => arr.indexOf(cat) === i).map((category) => (
+                        <option key={category} value={category}>
+                            {category}
+                        </option>
+                    ))}
+                </select>
+            </div>
             <Row className="justify-content-center align-items-center mt-4" style={{ height: '100%' }}>
                 {screenings.map(({ id, time, movieId, auditoriumId }) => (
                     <Col key={id} className="my-4">
-                        <Screening id={id} time={time} movieId={movieId} auditoriumId={auditoriumId} />
+                        <Screening
+                            id={id}
+                            time={time}
+                            movieId={movieId}
+                            movieTitle={movieTitles[movieId]}
+                            auditoriumId={auditoriumId}
+                            auditoriumName={auditoriumNames[auditoriumId]}
+                            category={categories[movieId]}
+                        />
                     </Col>
                 ))}
             </Row>
         </Container>
+
+
     );
 };
 
